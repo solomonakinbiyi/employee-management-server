6056871d974d5a90ea2d16cd4635c3d30028df39
solomonakinbiyi <akinbiyisolo22@gmail.com>
2022-11-30 09:45:35 +0100

    config file created

---
+++ src/config/config.ts
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const MONGO_USERNAME = process.env.MONGO_USERNAME || "";
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD || "";
+const MONGO_URL = "";
+
+const SERVER_PORT = process.env.SERVER_PORT
+  ? Number(process.env.SERVER_PORT)
+  : 1337;
+
+export const config = {
+    mongo: {
+        url: MONGO_URL,
+    },
+    server: {
+        port: SERVER_PORT,
+    }
+}
